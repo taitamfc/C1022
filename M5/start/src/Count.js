9e77693071ff89d533227f97f2802bb4d2fd8025
taitamfc <taitam8590@gmail.com>
2023-03-31 11:13:00 +0700

    state count

--- M5/start/src/Count.js
+++ M5/start/src/Count.js
@@ -4,17 +4,31 @@ class Count extends Component {
     constructor(props){
         super(props);
         // Khai bao state count
+        this.state = {
+            count:0  
+        }
     }
 
     // Phuong thuc xu ly click
+    setCount = () => {
+        // alert(123);
+        // this.setState( {
+        //     count: this.state.count + 1
+        // });
 
+        this.setState( prevState => ({
+            count: prevState.count + 1
+        }) );
+    }
     render() {
         return (
             <div>
                 <h1> Chuong trinh dem </h1>
                 {/* Hien thi so lan click */}
-                <h1></h1>
+                <h1>Hien thi so lan Click: {this.state.count}</h1>
                 {/* Tao button goi click va tang click len 1 dv */}
+                <button onClick={this.setCount}> tang len </button>
+
 
                 <hr></hr>
             </div>
