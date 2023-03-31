774d93082a90009827107b6e5949825dcd084c29
taitamfc <taitam8590@gmail.com>
2023-03-31 11:51:02 +0700

    Calculator

--- M5/start/src/Calculator.js
+++ M5/start/src/Calculator.js
@@ -3,26 +3,66 @@ import React, { Component } from 'react';
 class Calculator extends Component {
     constructor(props){
         super(props)
-
+            this.state = {
+                soA:0,
+                soB:0,
+                result:0
+            }
         // khai bao state: soA, soB, result
     }
 
     // Khai bao 4 PT cong,tru,nhan, chia
+    setSoA = (value) => {
+        this.setState( {soA:value} );
+    }
+    setSoB = (value) => {
+        this.setState( {soB:value} );
+    }
+    cong = () => {
+        let soA = parseInt(this.state.soA);
+        let soB = parseInt(this.state.soB);
+        let kq = soA + soB;
+        this.setState( {result:kq} );
+    }
+    tru = () =>{
+       let soA = parseInt(this.state.soA)
+       let soB = parseInt(this.state.soB)
+       let kq = soA - soB;
+        this.setState( {result:kq} );
+    }
+    nhan = ()=>{
+        let soA = parseInt(this.state.soA)
+        let soB = parseInt(this.state.soB)
+        let kq = soA * soB;
+        this.setState( {result:kq} );
 
+    }
+    chia = () =>{
+        let soA = parseInt(this.state.soA)
+        let soB = parseInt(this.state.soB)
+        let kq = soA / soB;
+        this.setState( {result:kq} );
+    }
     render() {
         return (
             <div>
-                <form>
-                    So thu nhat: <input type={'text'} /> <br/>
-                    So thu hai: <input type={'text'} /> <br/>
+                    <h1> {this.state.soA} - {this.state.soB} </h1>
+                    So thu nhat: 
+                    <input type={'text'} 
+                        onChange={ (e) => this.setSoA(e.target.value)} 
+                    /> <br/>
+                    So thu hai: 
+                    <input type={'text'} 
+                        onChange={ (e) => this.setSoB(e.target.value)} 
+                    /> <br/>
                     {/* Hien thi ket qua */}
-                    <p>Ket qua: 0</p>
+                    <p>Ket qua: { this.state.result }</p>
                     {/* Bat su kien 4 pt tuong ung */}
-                    <button> + </button>
-                    <button> - </button>
-                    <button> * </button>
-                    <button> / </button>
-                </form>
+                    <button onClick={this.cong}> + </button>
+                    <button onClick={this.tru}> - </button>
+                    <button onClick={this.nhan}> * </button>
+                    <button onClick={this.chia}> / </button>
+                    
             </div>
         );
     }
