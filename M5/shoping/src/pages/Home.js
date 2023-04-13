a9dcdbcc86555f226613f726055d6e738de02d95
taitamfc <taitam8590@gmail.com>
2023-04-13 10:49:43 +0700

    gan link

--- M5/shoping/src/pages/Home.js
+++ M5/shoping/src/pages/Home.js
@@ -24,12 +24,14 @@ function Home(props) {
                         <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
                         <div className="card-body p-4">
                             <div className="text-center">
-                                <h5 className="fw-bolder">Fancy Product</h5>
-                                $40.00 - $80.00
+                                <h5 className="fw-bolder">{product.name}</h5>
+                                {product.price}
                             </div>
                         </div>
                         <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                            <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                            <div className="text-center">
+                                <Link to={'/sanpham/'+product.id} className="btn btn-outline-dark mt-auto">Xem chi tiết</Link>
+                            </div>
                         </div>
                     </div>
                 </div>
