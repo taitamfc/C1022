7cebaa0cef4aa187d3f183f5460ce09b45040dcc
taitamfc <taitam8590@gmail.com>
2023-04-13 11:04:26 +0700

    product

--- M5/shoping/src/pages/SanPhamShow.js
+++ M5/shoping/src/pages/SanPhamShow.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import ProductModel from '../models/ProductModel';
 import LayoutMaster from '../layouts/LayoutMaster';
 
 function SanPhamShow(props) {
+    const {id} = useParams();
+    const [product,setProduct] = useState({});
+    // gọi API
+    useEffect( () => {
+        ProductModel.find(id).then(res => {
+            setProduct(res.data);
+        })
+        .catch(err => {
+            throw err;
+        });
+    },[] );
     return (
         <LayoutMaster>
             <div className="row gx-4 gx-lg-5 align-items-center">
                 <div className="col-md-6"><img className="card-img-top mb-5 mb-md-0" src="https://dummyimage.com/600x700/dee2e6/6c757d.jpg" alt="..." /></div>
                 <div className="col-md-6">
-                    <div className="small mb-1">SKU: BST-498</div>
-                    <h1 className="display-5 fw-bolder">Shop item template</h1>
+                    <div className="small mb-1">{ product.category }</div>
+                    <h1 className="display-5 fw-bolder">{ product.name }</h1>
                     <div className="fs-5 mb-5">
-                        <span className="text-decoration-line-through">$45.00</span>
-                        <span>$40.00</span>
+                        <span>{ product.price }</span>
                     </div>
                     <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?</p>
                     <div className="d-flex">
