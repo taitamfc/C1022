8b5268a49de020da81efbb38f93f1694eec71e81
taitamfc <taitam8590@gmail.com>
2023-04-13 11:25:26 +0700

    add to cart

--- M5/shoping/src/pages/SanPhamShow.js
+++ M5/shoping/src/pages/SanPhamShow.js
@@ -2,10 +2,16 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import ProductModel from '../models/ProductModel';
 import LayoutMaster from '../layouts/LayoutMaster';
+import { useDispatch, useSelector } from "react-redux";
+import { SET_CART } from '../redux/action';
 
 function SanPhamShow(props) {
+    const dispatch = useDispatch();
     const {id} = useParams();
     const [product,setProduct] = useState({});
+    const [qty,setQty] = useState(1);
+    const carts = useSelector( state => state.cart )
+
     // gọi API
     useEffect( () => {
         ProductModel.find(id).then(res => {
@@ -15,6 +21,21 @@ function SanPhamShow(props) {
             throw err;
         });
     },[] );
+
+    const handleAddToCart = () => {
+        const cart = {
+            id: id,
+            qty: qty
+
+        }
+        console.log(carts);
+        carts.push(cart);
+        dispatch({ type: SET_CART, payload: carts });
+    }
+    const handleQty = (event) => {
+        setQty(event.target.value);
+    }
+
     return (
         <LayoutMaster>
             <div className="row gx-4 gx-lg-5 align-items-center">
@@ -27,8 +48,8 @@ function SanPhamShow(props) {
                     </div>
                     <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?</p>
                     <div className="d-flex">
-                        <input className="form-control text-center me-3" id="inputQuantity" type="num" value="1" />
-                        <button className="btn btn-outline-dark flex-shrink-0" type="button">
+                        <input className="form-control text-center me-3" type="number" value={qty} onChange={ handleQty } />
+                        <button onClick={ handleAddToCart } className="btn btn-outline-dark flex-shrink-0" type="button">
                             <i className="bi-cart-fill me-1"></i>
                             Add to cart
                         </button>
